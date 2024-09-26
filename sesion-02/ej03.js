7704ab9afcefc658ba5f9372e14f33f97f7863ba
SebastianRoberto <sebastian.roberto.pp@gmail.com>
2024-09-26 18:07:52 +0200

    .

--- sesion-02/ej03.js
+++ sesion-02/ej03.js
@@ -1,16 +1,18 @@
 const usuario = {
     nombre: "Ana",
-    correo: "ana@example.com",
-   };
-   
-   console.log(`- ej03.js`);
-   console.log(`- ej03.js`);
-   console.log(``);
-   console.log(`>> SALIDA `);
-   console.log(`>> SALIDA `);
-   console.log(``);
-   
-   const { nombre, edad = 30 } = usuario;
+    correo: "ana@example.com"
+};
 
-   console.log(nombre); 
-   console.log(edad); 
+console.log(`- ej03.js`);  
+console.log(`- ej03.js`);  
+console.log(``);  
+console.log(`>> SALIDA `);  
+console.log(`>> SALIDA `);  
+console.log(``); 
+
+
+const {nombre, correo, edad = 30} = usuario;
+
+console.log(nombre);  
+console.log(correo);  
+console.log(edad);    
