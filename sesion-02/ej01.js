3dd0254fd44bfa8611cde2d44160c97f39a4588f
SebastianRoberto <sebastian.roberto.pp@gmail.com>
2024-09-26 16:38:28 +0200

    comentario

---
+++ sesion-02/ej01.js
@@ -0,0 +1,16 @@
+const persona = {
+    nombre: "Luis",
+    edad: 25,
+    profesion: "ingeniero",
+   };
+   
+   const { nombre, edad } = persona;
+   console.log(`- ej01.js`);
+   console.log(`- ej01.js`);
+   console.log(``);
+   console.log(`>> SALIDA `);
+   console.log(`>> SALIDA `);
+   console.log(``);
+   console.log(nombre); 
+   console.log(edad);
+   
