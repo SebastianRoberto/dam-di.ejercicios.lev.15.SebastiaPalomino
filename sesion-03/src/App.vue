545fc6ccea0741dc69f22fb4406dbf9b542afbad
Sebastian <sebastian.roberto.pp@gmail.com>
2024-10-09 20:58:11 +0200

    Imagen Estatica

--- sesion-03/src/App.vue
+++ sesion-03/src/App.vue
@@ -2,18 +2,21 @@
 import ListaEstatica  from   './components/ListaEstatica.vue';
 import MensajeEstatico  from   './components/MensajeEstatico.vue';
 import LibroInfo  from   './components/LibroInfo.vue';
+import ImagenEstatica from './components/ImagenEstatica.vue';
 
 </script>
 
 <template>
 
   <div>
-   EJ02-AP01: Aplicacion Inicializadda.
+   Ejercicios
 </div>
 
  <MensajeEstatico></MensajeEstatico>
  <ListaEstatica></ListaEstatica>
  <LibroInfo></LibroInfo>
+ <ImagenEstatica></ImagenEstatica>
+ 
 
 </template>
 
--- sesion-03/src/components/ImagenEstatica.vue
+++ sesion-03/src/components/ImagenEstatica.vue
@@ -1,21 +1,44 @@
-<script setup> 
-
-
-
+<template>
+    <div class="imagen-estatica">
+      <h2>Imagen Local</h2>
+      <img :src="imagenLocal" alt="Imagen Local" />
+  
+      <h2>Imagen Externa</h2>
+      <img :src="imagenExterna" alt="Imagen Externa" />
+    </div>
+  </template>
+  
+  <script>
+import imagenLocal from '@/assets/imagen-local.png'; 
+export default {
+  name: 'ImagenEstatica',
+  data() {
+    return {
+      imagenLocal: imagenLocal,
+      imagenExterna: 'https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/9db5/live/48fd9010-c1c1-11ee-9519-97453607d43e.jpg.webp'
+    };
+  }
+};
 </script>
 
-
-<template>
-    
-
-    <div>
-        <img :src="C:\Users\USUARIO\Downloads\tuuu.jfif" width="300" />
-
-        <img :src="externalImage" alt="Imagen Externa" width="300" />
-    </div>
-</template>
-
-
-<style scope>
-
-</style>
+  
+  <style scoped>
+  .imagen-estatica {
+    text-align: center;
+    margin: 20px;
+  }
+  
+  .imagen-estatica img {
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 20px;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+  }
+  
+  .imagen-estatica h2 {
+    margin-bottom: 10px;
+    color: #333;
+  }
+  </style>
+  
